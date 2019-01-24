303020507560036d87e904371e3edb77b89d7bf3
Deliman206 <smiller206@me.com>
2019-01-23 21:01:12 -0800

    finished solution and added css

--- Lab11MVC/Lab11MVC/Controllers/HomeController.cs
+++ Lab11MVC/Lab11MVC/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab11MVC.Models;
 
 namespace Lab11MVC.Controllers
 {
     public class HomeController: Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int startYear, int endYear)
+        {
+            return RedirectToAction("Results", new { startYear, endYear });
+        }
+
+        [HttpGet]
+        public IActionResult Results(int startYear, int endYear)
+        {
+            return View(TimePerson.GetPersons(startYear, endYear));
         }
     }
 }
---
+++ Lab11MVC/Lab11MVC/Models/TimePerson.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11MVC.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+        public TimePerson(int year, string honor, string name, string country, int birthYear, int deathYear, string title, string category, string context)
+        {
+            Year = year;
+            Honor = honor;
+            Name = name;
+            Country = country;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+            Title = title;
+            Category = category;
+            Context = context;
+        }
+
+        public static List<TimePerson> GetPersons(int startYear, int endYear)
+        {
+            string filepath = @"\\mac\Home\codefellows\dn401\Lab11-FirstMvcApp\Lab11MVC\Lab11MVC\wwwroot\personOfTheYear.csv";
+            List <string> allPeople = Collection(filepath);
+            
+            var query = from line in allPeople
+                        let data = line.Split(',')
+                        where data[0] != "Year"
+                        select new TimePerson(
+                            Convert.ToInt32(data[0]),
+                            data[1],
+                            data[2],
+                            data[3],
+                            (data[4] == "") ? 0 : Convert.ToInt32(data[4]),
+                            (data[5] == "") ? 0 : Convert.ToInt32(data[5]),
+                            data[6],
+                            data[7],
+                            data[8]
+                            );
+
+            List<TimePerson> requestedPeople = new List<TimePerson>();
+
+            foreach (TimePerson person in query)
+            {
+                if (person.Year >= startYear && person.Year <= endYear)
+                    requestedPeople.Add(person);
+            }
+
+            return requestedPeople;
+        }
+        static List<string> Collection(string filepath)
+        {
+            var reader = new StreamReader(File.OpenRead(filepath));
+            List<string> lines = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                lines.Add(line);
+            }
+            return lines;
+        }
+    }
+}
